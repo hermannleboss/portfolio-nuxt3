1ab4d5c09f9335536b1181b38982cd9fe80bb2f5
Hermann EHO <hermannleboss@gmail.com>
2023-08-30 18:27:01 +0000

    chore: add api key setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import process from "process";
+
 export default defineNuxtConfig({
+  modules: [
+    ["@storyblok/nuxt", {accessToken: process.env.ACCESS_TOKEN}]
+  ],
   devtools: { enabled: true }
 })
