bb287d06c637e09be6f6c2bd19f0f6c4d4c2d38d
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 23:45:45 +0000

    feat: containerise layout

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <NavBar></NavBar>
-  <NuxtPage></NuxtPage>
+  <div class="min-h-screen bg-very-light-gray text-grayish-dark-blue" id="main" >
+    <NavBar></NavBar>
+    <div class="container mx-auto px-4">
+      <NuxtPage></NuxtPage>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
