9e793325b05cb2cab47e4f6faa37c323de30a7e8
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 15:29:34 +0000

    feat: button component

---
+++ components/Button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="inline-flex bg-dark-blue hover:bg-dark-blue text-white font-bold py-2 px-4 ">
+    <span class="flex items-center justify-center">
+      <svg
+        fill="none"
+        height="14"
+        viewBox="0 0 18 14"
+        width="18"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M1 9L9 13L17 9" stroke="#FFF"/>
+        <path d="M1 5L9 9L17 5" opacity="0.5" stroke="#FFF"/>
+        <path d="M1 1L9 5L17 1" opacity="0.25" stroke="#FFF"/>
+      </svg></span>
+    Button
+  </div>
+</template>
+
+<style scoped>
+
+</style>
