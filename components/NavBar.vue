e73ca49d95c382b32e0a2ac3bda3f210bf38fc81
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 16:38:17 +0000

    feat: create nav bar

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg
+      class="nav-logo"
+      fill="none"
+      viewBox="0 0 61 32"
+      xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+        clip-rule="evenodd"
+        d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z"
+        fill="#33323D"
+        fill-rule="evenodd"
+    />
+  </svg>
+
+</template>
+
+<style scoped>
+
+</style>
