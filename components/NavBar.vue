dbd34e45f1a474af805c34636679f2ad66313cd6
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 23:46:07 +0000

    feat: structure navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,22 +3,29 @@
 </script>
 
 <template>
-  <svg
-      class="nav-logo"
-      fill="none"
-      viewBox="0 0 61 32"
-      xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-        clip-rule="evenodd"
-        d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z"
-        fill="#33323D"
-        fill-rule="evenodd"
-    />
-  </svg>
+  <div class="container mx-auto px-4">
+
+    <svg
+        class="nav-logo"
+        fill="none"
+        viewBox="0 0 61 32"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+          clip-rule="evenodd"
+          d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z"
+          fill="#33323D"
+          fill-rule="evenodd"
+      />
+    </svg>
+  </div>
 
 </template>
 
 <style scoped>
 
+.nav-logo {
+  width: 61px;
+  height: 32px;
+}
 </style>
