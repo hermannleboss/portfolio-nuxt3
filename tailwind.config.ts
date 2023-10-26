ecdccca673042fc44819efd5f19b845db8ece2e4
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 15:25:07 +0000

    feat: disable viewer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,27 @@
-import type { Config } from 'tailwindcss'
-
+import type {Config} from 'tailwindcss'
+// import tailwind colors
+import colors from 'tailwindcss/colors'
 export default {
-  content: [],
-  theme: {
-    colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'cyan': '#5FB4A2',
-        'dark-blue': '#203A4C',
-        'grayish-dark-blue': '#33323D',
-        'very-light-gray': '#D7D7D7',
-        'light-gray': '#EAEAEB',
-        'bright-red': '#F43030',
+    content: [],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'cyan': '#5FB4A2',
+            'dark-blue': '#203A4C',
+            'grayish-dark-blue': '#33323D',
+            'very-light-gray': '#FAFAFA',
+            'light-gray': '#EAEAEB',
+            'bright-red': '#F43030',
+            white: colors.white,
+            green: colors.green,
+        },
+        fontFamily: {
+            sans: ['Public Sans', 'sans-serif'],
+            serif: ['Ibarra Real Nova', 'serif'],
+        },
+        extend: {},
     },
-      fontFamily: {
-          sans: ['Public Sans', 'sans-serif'],
-          serif: ['Ibarra Real Nova', 'serif'],
-      },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config
 
