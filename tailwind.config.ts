0331cac945cc121c5d36152138527d831a2c7e68
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 14:33:56 +0000

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,14 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [],
   theme: {
+    colors: {
+        'cyan': '#5FB4A2',
+        'dark-blue': '#203A4C',
+        'grayish-dark-blue': '#33323D',
+        'very-light-gray': '#D7D7D7',
+        'light-gray': '#EAEAEB',
+        'bright-red': '#F43030',
+    },
     extend: {},
   },
   plugins: [],
