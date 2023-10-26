e47801bca4cb22f3aba9e7dca7b0f5e5dafa79a0
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 16:14:13 +0000

    feat: handle disabled state

--- components/Button.vue
+++ components/Button.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import {defineProps} from 'vue'
+
 const props = defineProps({
   variant: {type: String, default: 'primary'},
+  disabled: {type: Boolean, default: false}
 })
 
 </script>
 
 <template>
-  <div class="inline-flex cursor-pointer  font-bold"
-       :class="variant==='secondary' ? 'border hover:bg-grayish-dark-blue hover:text-white' : ' bg-dark-blue text-white hover:bg-cyan '">
+  <div class="inline-flex  font-bold"
+       :class="disabled ? (variant==='secondary' ? 'border border-light-gray' : 'bg-light-gray') : (variant==='secondary' ? 'cursor-pointer border hover:bg-grayish-dark-blue hover:text-white' : 'cursor-pointer bg-dark-blue text-white hover:bg-cyan ')"
+  >
     <slot name="icon">
     </slot>
     <span class="px-12 py-4" style="font-size: 0.75rem; letter-spacing: 0.125rem">
