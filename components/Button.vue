220fe795fb74b0929d49863cbdd87bcff375b1d2
Hermann EHO <hermannleboss@gmail.com>
2023-10-26 15:44:42 +0000

    feat: button style updated

--- components/Button.vue
+++ components/Button.vue
@@ -3,20 +3,22 @@
 </script>
 
 <template>
-  <div class="inline-flex bg-dark-blue hover:bg-dark-blue text-white font-bold py-2 px-4 ">
-    <span class="flex items-center justify-center">
+  <div class="inline-flex bg-dark-blue hover:bg-dark-blue text-white font-bold">
+    <span class="flex items-center justify-center opacity-10 bg-black  p-4">
       <svg
-        fill="none"
-        height="14"
-        viewBox="0 0 18 14"
-        width="18"
-        xmlns="http://www.w3.org/2000/svg"
-    >
+          fill="none"
+          height="14"
+          viewBox="0 0 18 14"
+          width="18"
+          xmlns="http://www.w3.org/2000/svg"
+      >
         <path d="M1 9L9 13L17 9" stroke="#FFF"/>
         <path d="M1 5L9 9L17 5" opacity="0.5" stroke="#FFF"/>
         <path d="M1 1L9 5L17 1" opacity="0.25" stroke="#FFF"/>
       </svg></span>
-    Button
+    <span class="px-12 py-4" style="font-size: 0.75rem; letter-spacing: 0.125rem">
+       ABOUT ME
+    </span>
   </div>
 </template>
 
