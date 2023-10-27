f337256e17bfc68db15b65df2143ea641165b344
Hermann EHO <hermannleboss@gmail.com>
2023-10-27 11:04:42 +0000

    feat: Presentation CARD PMV

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
+import PresentationCard from "~/components/PresentationCard.vue";
+const presentation= {imageUrl: '~/assets/images/homepage/desktop/image-homepage-hero.jpg'}
 </script>
 
 <template>
-
+  <PresentationCard :presentation="presentation"></PresentationCard>
 </template>
 
 <style scoped>
