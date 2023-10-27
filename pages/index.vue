3ab7902c64882467e70a8f3622039114f6709cf0
Hermann EHO <hermannleboss@gmail.com>
2023-10-27 00:18:02 +0000

    feat: create hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 
+import IconComponent from "~/components/IconComponent.vue";
 </script>
 
 <template>
-  <div class="bg-black">
+  <div class="bg-black relative">
     <img
         alt="image"
         class="hidden xl:block w-full"
@@ -19,9 +20,33 @@
         class="block md:hidden w-full"
         src="~/assets/images/homepage/mobile/image-homepage-hero.jpg"
     />
+
+    <div class="hero-text sm:absolute border border-white bg-white bottom-0 left-0">
+      <h1 class="pb-4 title-1 font-serif text-black font-bold">
+        Hey, I’m Hermann EHO and I love building beautiful websites
+      </h1>
+      <Button>
+        <template #icon>
+          <IconComponent/>
+        </template>
+        About Me
+      </Button>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.hero {
+  &-text {
+    @media (min-width: 640px) {
+      padding-top: 55px;
+      padding-right: 55px;
+      width: 514px;
+    }
 
+    @media (min-width: 1280px) {
+      width: 445px;
+    }
+  }
+}
 </style>
