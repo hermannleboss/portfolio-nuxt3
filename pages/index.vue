912f0ca0da7b53fbc5dac865c41a5f4d11b6fce4
Hermann EHO <hermannleboss@gmail.com>
2023-10-27 00:06:42 +0000

    feat: handle image responsive

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-Index Page
+  <div class="bg-black">
+    <img
+        alt="image"
+        class="hidden xl:block w-full"
+        src="~/assets/images/homepage/desktop/image-homepage-hero.jpg"
+    />
+    <img
+        alt="image"
+        class="hidden md:block xl:hidden w-full"
+        src="~/assets/images/homepage/tablet/image-homepage-hero.jpg"
+    />
+    <img
+        alt="image"
+        class="block md:hidden w-full"
+        src="~/assets/images/homepage/mobile/image-homepage-hero.jpg"
+    />
+  </div>
 </template>
 
 <style scoped>
