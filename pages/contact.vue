7403ed17355c46fa933f62cfe6a9f74e8c8f2830
Hermann EHO <hermannleboss@gmail.com>
2023-11-02 11:54:38 +0000

    feat: contact page content

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <div class="block md:flex m-0 border-y border-light-gray pt-12 mt-8">
+    <div class="flex-1">
+      <h2 class="title-2 pt-3 py-sm-4 pb-4 m-0">Get in touch</h2></div>
+    <div class="flex-1">
+      <p>
+        I’d love to hear about what you’re working on and how I could help. I’m currently looking for a new role and am
+        open to a wide range of opportunities. My preference would be to find a position in a company in London. But I’m
+        also happy to hear about opportunites that don’t fit that description. I’m a hard-working and positive person
+        who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check
+        out my online profiles below and get in touch using the form.
+      </p>
+      <div class="flex">
+        <NuxtLink to="https://github.com/hermannleboss" class="p-4">
+          <img
+              alt="Github Icon"
+              class="img-full mx-auto"
+              src="~/assets/images/icons/github.svg">
+        </NuxtLink>
+        <NuxtLink  to="https://twitter.com/hermanneho" class="p-4">
+          <img
+              alt="Twitter Icon"
+              class="img-full mx-auto"
+              src="~/assets/images/icons/twitter.svg">
+        </NuxtLink>
+        <NuxtLink  to="https://www.linkedin.com/in/hermann-eho/" class="p-4">
+          <img
+              alt="Linkedin Icon"
+              class="img-full mx-auto"
+              src="~/assets/images/icons/linkedin.svg">
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 
 </template>
-
-<style scoped>
-
-</style>
