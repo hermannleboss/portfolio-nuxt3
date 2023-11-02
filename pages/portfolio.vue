ff417df14d360e6eb45c872d62c2b35118bc6491
Hermann EHO <hermannleboss@gmail.com>
2023-11-02 11:31:39 +0000

    feat: use presentation card on portfolio page

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -5,7 +5,18 @@ const presentation= {imageUrl: '~/assets/images/homepage/desktop/image-homepage-
 </script>
 
 <template>
-  <PresentationCard :presentation="presentation"></PresentationCard>
+  <div class="flex flex-col gap-20">
+    <PresentationCard :presentation="presentation"></PresentationCard>
+    <PresentationCard :presentation="presentation" variant="secondary"></PresentationCard>
+  </div>
+  <div class="my-36 flex items-center">
+    <h3 class="title-2 max-w-sm pr-6">Interested in doing a project together?</h3>
+    <span class="flex-1 border border-light-gray-2 h-0 mx-8"></span>
+
+    <Button variant="secondary" to="/realisation" class="my-4">
+      CONTACT ME
+    </Button>
+  </div>
 </template>
 
 <style scoped>
