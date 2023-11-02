629acd7d66c02ae09e3073d2eba57bad5e7662fc
Hermann EHO <hermannleboss@gmail.com>
2023-11-02 14:23:41 +0000

    feat: handle title padding

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-36 flex flex-col gap-10 items-center">
-    <h3 class="title-2 font-serif max-w-sm">Interested in doing a project together?</h3>
+    <h3 class="title-2 font-serif text-center max-w-sm px-3">Interested in doing a project together?</h3>
     <span class="hidden flex-1 border border-light-gray-2 h-0 mx-8"></span>
     <Button variant="secondary" to="/contact" class="sm:my-4">
       CONTACT ME
