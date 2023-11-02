31030d04071dea32c0934586b3bd904ebebc8b4b
Hermann EHO <hermannleboss@gmail.com>
2023-11-02 12:14:15 +0000

    feat: nav and footer links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,19 +5,21 @@
 <template>
   <div class="container-fluid">
     <div class="py-8 flex justify-between">
-      <svg
-          class="nav-logo"
-          fill="none"
-          viewBox="0 0 61 32"
-          xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-            clip-rule="evenodd"
-            d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z"
-            fill="#33323D"
-            fill-rule="evenodd"
-        />
-      </svg>
+
+      <NuxtLink to="/">
+        <svg
+            class="nav-logo"
+            fill="none"
+            viewBox="0 0 61 32"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+              clip-rule="evenodd"
+              d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z"
+              fill="#33323D"
+              fill-rule="evenodd"
+          />
+        </svg></NuxtLink>
       <div>
         <div class="flex gap-12">
           <NuxtLink to="/" :class="$route.path === '/' ? 'text-cyan': ''">HOME</NuxtLink>
@@ -34,8 +36,6 @@
 
 a{
   font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
   line-height: normal;
   letter-spacing: 2px;
   text-transform: uppercase;
