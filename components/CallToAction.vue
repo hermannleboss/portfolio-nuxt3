28b1ca7885656b7af55ac78781661082b4daff06
Hermann EHO <hermannleboss@gmail.com>
2023-11-02 12:04:46 +0000

    feat: call to action feature

---
+++ components/CallToAction.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="my-36 flex items-center">
+    <h3 class="title-2 font-serif max-w-sm">Interested in doing a project together?</h3>
+    <span class="flex-1 border border-light-gray-2 h-0 mx-8"></span>
+    <Button variant="secondary" to="/contact" class="my-4">
+      CONTACT ME
+    </Button>
+  </div>
+</template>
