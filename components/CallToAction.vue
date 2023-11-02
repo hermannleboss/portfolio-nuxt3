50752168b6e2c24f41559e23bd926204d77c4899
Hermann EHO <hermannleboss@gmail.com>
2023-11-02 14:22:36 +0000

    feat: call to action perfect for mobile

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="my-36 flex items-center">
+  <div class="my-36 flex flex-col gap-10 items-center">
     <h3 class="title-2 font-serif max-w-sm">Interested in doing a project together?</h3>
-    <span class="flex-1 border border-light-gray-2 h-0 mx-8"></span>
-    <Button variant="secondary" to="/contact" class="my-4">
+    <span class="hidden flex-1 border border-light-gray-2 h-0 mx-8"></span>
+    <Button variant="secondary" to="/contact" class="sm:my-4">
       CONTACT ME
     </Button>
   </div>
