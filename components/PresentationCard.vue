34c47e9e443953f49f2a844fec61d2ac0ac3b6b3
Hermann EHO <hermannleboss@gmail.com>
2023-11-05 21:23:30 +0000

    feat: PresentationCard.vue responsive

--- components/PresentationCard.vue
+++ components/PresentationCard.vue
@@ -13,13 +13,15 @@ const props = defineProps({
 </script>
 
 <template>
-  <div :class="props.variant==='secondary'? 'flex-row-reverse': ''" class="block md:flex m-0">
-    <img
-        alt="Profile image"
-        class="img-full mx-auto card-image"
-        src="~/assets/images/portfolio/desktop/image-portfolio-bookmark.jpg"
-    />
-    <div class="flex justify-between about m-0 sm:mx-24 min-h-full">
+  <div :class="props.variant==='secondary'? 'flex-row-reverse': ''" class="block md:flex m-0 sm:gap-16">
+    <div class=" w-full">
+      <img
+          alt="Profile image"
+          class="img-full mx-auto card-image"
+          src="~/assets/images/portfolio/desktop/image-portfolio-bookmark.jpg"
+      />
+    </div>
+    <div class="flex justify-between about m-0 lg:mx-24 min-h-full md:max-w-[280px]">
       <div class="border-y border-light-gray mt-8 md:mt-0 flex items-center">
         <div>
           <h2 class="title-2 font-serif py-8">TITLE {{ presentation.title }}</h2>
