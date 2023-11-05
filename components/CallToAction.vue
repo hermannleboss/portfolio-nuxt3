cfe0ed4b7a356eacccd7deae10ac9e6adadbfa9d
Hermann EHO <hermannleboss@gmail.com>
2023-11-05 20:46:07 +0000

    feat: call to action responsive

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="my-36 flex flex-col gap-10 items-center">
-    <h3 class="title-2 font-serif text-center max-w-sm px-3">Interested in doing a project together?</h3>
-    <span class="hidden flex-1 border border-light-gray-2 h-0 mx-8"></span>
+  <div class="mt-32 mb-20 sm:my-24 md:my-36 flex flex-col sm:flex-row gap-10 sm:gap-8 items-center">
+    <h3 class="title-2 font-serif text-center sm:text-left max-w-[350px]">Interested in doing a project together?</h3>
+    <span class="hidden sm:flex flex-1 border border-light-gray-2 h-0"></span>
     <Button variant="secondary" to="/contact" class="sm:my-4">
       CONTACT ME
     </Button>
