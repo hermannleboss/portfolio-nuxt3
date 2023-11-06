9080a3c43457c371e60dc12dbe51f9ee351079b1
Hermann EHO <hermannleboss@gmail.com>
2023-11-06 10:18:44 +0000

    feat: finish id presentation

--- pages/portfolio/[id].vue
+++ pages/portfolio/[id].vue
@@ -9,49 +9,53 @@ const presentation = {imageUrl: '~/assets/images/homepage/desktop/image-homepage
       class="w-full"
       src="~/assets/images/detail/desktop/image-bookmark-hero.jpg"
   />
-  <div class="py-6 mt-10 border-y border-light-gray block md:grid md:grid-cols-2 lg:block">
-    <div class="flex flex-col gap-6 items-start">
-      <h2 class="title-2 font-serif">Manage</h2>
-      <p class="md:hidden lg:block">
-        This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along
-        with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.
-      </p>
-      <p>Interaction Design / Front End Development
-        <br>HTML / CSS / JS</p>
-      <Button variant="secondary" :to="presentation.url">
-        VIEW WEBSITE
-      </Button>
+  <div class="lg:flex justify-between lg:gap-8">
+    <div class="py-6 mt-10 border-y border-light-gray block md:grid md:grid-cols-2 lg:block mdmax-w-[350px]">
+      <div class="flex flex-col gap-6 items-start">
+        <h2 class="title-2 font-serif">Manage</h2>
+        <p class="md:hidden lg:block">
+          This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along
+          with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.
+        </p>
+        <p>Interaction Design / Front End Development
+          <br>HTML / CSS / JS</p>
+        <Button variant="secondary" :to="presentation.url">
+          VIEW WEBSITE
+        </Button>
+      </div>
+      <div class="hidden md:block lg:hidden">
+        <p>
+          This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along
+          with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.
+        </p>
+      </div>
     </div>
-    <div class="hidden md:block lg:hidden">
-      <p>
-        This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along
-        with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.
-      </p>
-    </div>
-  </div>
-  <div class="gap-6 py-6">
-    <h2 class="title-2 font-light font-serif mt-12 mb-7">Project Background</h2>
-    <p>
-      This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building
-      websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website
-      should look at different screen sizes. Creating these projects has helped me refine my workflow and solve
-      real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my
-      style.
-    </p>
-  </div>
-  <div>
-    <h2 class="title-2 font-light font-serif my-10">Static Previews</h2>
-    <div class="flex flex-col gap-8">
-      <img
-          alt="image"
-          class="w-full"
-          src="~/assets/images/detail/desktop/image-bookmark-preview-1.jpg"
-      />
-      <img
-          alt="image"
-          class="w-full"
-          src="~/assets/images/detail/desktop/image-bookmark-preview-2.jpg"
-      />
+    <div class="lg:max-w-[635px]">
+      <div class="gap-6 py-6">
+        <h2 class="title-2 font-light font-serif mt-12 mb-7">Project Background</h2>
+        <p>
+          This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building
+          websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website
+          should look at different screen sizes. Creating these projects has helped me refine my workflow and solve
+          real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my
+          style.
+        </p>
+      </div>
+      <div>
+        <h2 class="title-2 font-light font-serif my-10">Static Previews</h2>
+        <div class="flex flex-col gap-8">
+          <img
+              alt="image"
+              class="w-full"
+              src="~/assets/images/detail/desktop/image-bookmark-preview-1.jpg"
+          />
+          <img
+              alt="image"
+              class="w-full"
+              src="~/assets/images/detail/desktop/image-bookmark-preview-2.jpg"
+          />
+        </div>
+      </div>
     </div>
   </div>
   <div class="border-y border-light-gray grid grid-cols-2 divide-x divide-light-gray mt-16 sm:mt-20 md:mt-16">
