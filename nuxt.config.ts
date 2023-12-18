92af99f87c7a1dda133b01e86e9407ff29679234
Hermann EHO <hermannleboss@gmail.com>
2023-12-18 18:00:17 +0000

    feat: remove unnecessary config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: false},
-    ssr: true,
+    devtools: {enabled: true},
     css: ['~/assets/css/main.scss'],
     modules: ['@nuxtjs/tailwindcss', "@nuxt/image", '@pinia/nuxt'],
     tailwindcss: {
         viewer: false,
-    },
-    image: {
-        dir: 'assets',
-        provider: 'netlify',
-        netlify: {
-            baseURl: process.env.IMAGES_URL
-        }
     }
 })
