8ac71120992fbf37b23a559a4f75d3f680a3c083
Hermann EHO <hermannleboss@gmail.com>
2024-11-17 14:56:29 +0000

    feat: update content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+<<<<<<< Updated upstream
     devtools: {enabled: false},
+=======
+    app: {
+        head: {
+            charset: 'utf-8',
+            title: 'Hermann EHO | Web3 Developer',
+            htmlAttrs: {
+                lang: 'en'
+            }
+        }
+    },
+    devtools: {enabled: true},
+>>>>>>> Stashed changes
     css: ['~/assets/css/main.scss'],
     modules: ['@nuxtjs/tailwindcss', "@nuxt/image", '@pinia/nuxt'],
     tailwindcss: {
